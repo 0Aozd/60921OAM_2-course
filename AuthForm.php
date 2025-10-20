72a79c82c420fa4f0e505f064aabae047c81d28f
GROMITREPET <100571534+GROMITREPET@users.noreply.github.com>
2025-10-20 21:14:01 +0500

    Lesson1

--- AuthForm.php
+++ AuthForm.php
@@ -1,15 +1,14 @@
 <?php
-$cookie_lifetime = 12;
+$cookie_lifetime = 12; // время жизни cookie (в секундах)
 $uploadDir = "UserData/";
 
-
-// Обработка POST
+// --- Обработка отправки формы ---
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     // Сохраняем все поля в cookie
     foreach ($_POST as $key => $value) {
         if (is_array($value)) {
-            setcookie($key, implode(",", $value), time() + $cookie_lifetime);
+            setcookie($key, implode(", ", $value), time() + $cookie_lifetime);
         } else {
             setcookie($key, $value, time() + $cookie_lifetime);
         }
@@ -24,40 +23,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         }
     }
 
-    // Redirect после POST
-    header("Location: ".$_SERVER['PHP_SELF']);
+    // Перезагрузка страницы (чтобы избежать повторной отправки формы)
+    header("Location: " . $_SERVER["PHP_SELF"]);
     exit;
 }
 
-//Получаем данные из cookie
-$formData = [];
-$data = ['username','email','password','birthdate','gender','interests','currencies','about'];
-foreach($data as $d){
-    if(isset($_COOKIE[$d])){
-        if(in_array($d, ['interests','currencies'])) $formData[$d] = explode(",", $_COOKIE[$d]);
-        else $formData[$d] = $_COOKIE[$d];
-
-        if (is_array($formData[$d])) {
-            setcookie($d, implode(",", $formData[$d]), time() + $cookie_lifetime);
-        } else {
-            setcookie($d, $formData[$d], time() + $cookie_lifetime);
-        }
-    }
-}
-
-// Продлеваем cookie для загруженного файла
-$uploadedFilePath = $_COOKIE['uploadedFile'] ?? "";
-if($uploadedFilePath) setcookie("uploadedFile", $uploadedFilePath, time() + $cookie_lifetime);
-
-// Функции для удобного получения значений
-function getVal($name, $formData){
-    return $formData[$name] ?? '';
-}
-function getArr($name, $formData){
-    return $formData[$name] ?? [];
+// --- Продление срока cookie при каждом обновлении ---
+foreach ($_COOKIE as $key => $value) {
+    setcookie($key, $value, time() + $cookie_lifetime);
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -72,37 +47,36 @@ function getArr($name, $formData){
 <form action="" method="post" enctype="multipart/form-data">
 
     <label>Имя пользователя:</label><br>
-    <input type="text" name="username" required value="<?= getVal('username', $formData) ?>"><br>
+    <input type="text" name="username" required value="<?= $_COOKIE['username'] ?? '' ?>"><br>
 
     <label>Email:</label><br>
-    <input type="email" name="email" required value="<?= getVal('email', $formData) ?>"><br>
+    <input type="email" name="email" required value="<?= $_COOKIE['email'] ?? '' ?>"><br>
 
     <label>Пароль:</label><br>
-    <input type="password" name="password" value="<?= getVal('password', $formData) ?>"><br><br>
+    <input type="password" name="password" value="<?= $_COOKIE['password'] ?? '' ?>"><br><br>
 
     <label>Дата рождения:</label><br>
-    <input type="date" name="birthdate" value="<?= getVal('birthdate', $formData) ?>"><br><br>
+    <input type="date" name="birthdate" value="<?= $_COOKIE['birthdate'] ?? '' ?>"><br><br>
 
     <label>Загрузите фото профиля:</label><br>
     <input type="file" name="photo"><br><br>
 
     <label>Пол:</label><br>
     <?php
-    $genders = ["Мужской","Женский"];
-    $userGender = getVal('gender', $formData);
-
-    foreach($genders as $g){
-        $checked = ($userGender !== "" && $g === $userGender) ? "checked" : "";
+    $genders = ["Мужской", "Женский"];
+    $gender = $_COOKIE['gender'] ?? '';
+    foreach ($genders as $g) {
+        $checked = ($g === $gender) ? "checked" : "";
         echo "<input type='radio' name='gender' value='$g' $checked> $g<br>";
     }
     ?><br>
 
     <label>Интересы:</label><br>
     <?php
-    $interests = ["Финансы","Инвестиции","Путешествия"];
-    $interestsSelected = getArr('interests', $formData);
-    foreach($interests as $i){
-        $checked = in_array($i, $interestsSelected) ? "checked" : "";
+    $interestsAll = ["Финансы", "Инвестиции", "Путешествия"];
+    $interests = isset($_COOKIE['interests']) ? explode(", ", $_COOKIE['interests']) : [];
+    foreach ($interestsAll as $i) {
+        $checked = in_array($i, $interests) ? "checked" : "";
         echo "<input type='checkbox' name='interests[]' value='$i' $checked> $i<br>";
     }
     ?><br>
@@ -110,36 +84,36 @@ function getArr($name, $formData){
     <label>Валюты:</label><br>
     <select name="currencies[]" multiple size="3">
         <?php
-        $currencies = ["RUB"=>"Рубли","USD"=>"Доллары","EUR"=>"Евро","JPY"=>"Иена","CNY"=>"Юани"];
-        $currSelected = getArr('currencies', $formData);
-        foreach($currencies as $code=>$name){
-            $sel = in_array($code,$currSelected) ? "selected" : "";
+        $currencies = ["RUB" => "Рубли", "USD" => "Доллары", "EUR" => "Евро", "JPY" => "Иена", "CNY" => "Юани"];
+        $currSelected = isset($_COOKIE['currencies']) ? explode(", ", $_COOKIE['currencies']) : [];
+        foreach ($currencies as $code => $name) {
+            $sel = in_array($code, $currSelected) ? "selected" : "";
             echo "<option value='$code' $sel>$name</option>";
         }
         ?>
     </select><br><br>
 
     <label>О себе:</label><br>
-    <textarea name="about" rows="5"><?= getVal('about', $formData) ?></textarea><br><br>
+    <textarea name="about" rows="5"><?= $_COOKIE['about'] ?? '' ?></textarea><br><br>
 
     <button type="submit">Зарегистрироваться</button>
 </form>
 
-<?php if (!empty($formData)): ?>
+<?php if (!empty($_COOKIE['username'])): ?>
     <hr>
     <h3>Введённые данные:</h3>
-    <p><b>Имя:</b> <?= $formData["username"] ?></p>
-    <p><b>Email:</b> <?= $formData["email"] ?></p>
-    <p><b>Дата рождения:</b> <?= $formData["birthdate"] ?? "" ?></p>
-    <p><b>Пол:</b> <?= $formData["gender"] ?? "" ?></p>
-    <p><b>Интересы:</b> <?= implode(", ", $interestsSelected) ?></p>
-    <p><b>Валюты:</b> <?= implode(", ", $currSelected) ?></p>
-    <p><b>О себе:</b> <?= isset($formData["about"]) ? nl2br($formData["about"]) : "" ?></p>
+    <p><b>Имя:</b> <?= $_COOKIE["username"] ?></p>
+    <p><b>Email:</b> <?= $_COOKIE["email"] ?></p>
+    <p><b>Дата рождения:</b> <?= $_COOKIE["birthdate"] ?? "" ?></p>
+    <p><b>Пол:</b> <?= $_COOKIE["gender"] ?? "" ?></p>
+    <p><b>Интересы:</b> <?= $_COOKIE["interests"] ?? "" ?></p>
+    <p><b>Валюты:</b> <?= $_COOKIE["currencies"] ?? "" ?></p>
+    <p><b>О себе:</b> <?= nl2br($_COOKIE["about"] ?? "") ?></p>
 
-    <?php if (!empty($uploadedFilePath)): ?>
+    <?php if (!empty($_COOKIE['uploadedFile'])): ?>
         <p><b>Загруженное фото:</b></p>
-        <img src="<?= $uploadedFilePath ?>" width="150" style="border-radius: 10px;">
-    <?php endif;?>
+        <img src="<?= $_COOKIE['uploadedFile'] ?>" width="150" style="border-radius: 10px;">
+    <?php endif; ?>
 <?php endif; ?>
 
 </body>
